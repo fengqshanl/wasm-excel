688912dc015b5cb51cf8ffc43e8345d39189b92f
zhangz <zhangz@hiysg.com>
2023-09-19 08:54:26 +0800

    add

--- src/event/container_scroll.rs
+++ src/event/container_scroll.rs
@@ -1,16 +1,13 @@
-use web_sys::Element;
+use web_sys::{ Element, Event, HtmlDivElement };
+use wasm_bindgen::prelude::*;
+use crate::log;
 
 pub fn container_scroll_init(element: &Element) {
-  let set_page = Closure::<dyn FnMut(Event)>::new(move |event: Event| {
+  let scroll_event = Closure::<dyn FnMut(Event)>::new(move |event: Event| {
     let target = event.target().unwrap();
-    let tar = wasm_bindgen::JsCast::dyn_ref::<web_sys::HtmlInputElement>(&target).expect("msg");
-    let file_list: web_sys::FileList = HtmlInputElement::files(tar).expect("msg");
-    for i in 0..file_list.length() {
-      if let Some(file) = file_list.item(i) {
-        analyzer::analyzer(file);
-      }
-    }
+    let tar = wasm_bindgen::JsCast::dyn_ref::<web_sys::HtmlDivElement>(&target).expect("msg");
+    log(&format!("scroll_top: {}, scroll_left: {}", tar.scroll_top(), tar.scroll_left()));
   });
-  val.add_event_listener_with_callback("scroll", set_page.as_ref().unchecked_ref()).unwrap();
-  set_page.forget();
+  element.add_event_listener_with_callback("scroll", scroll_event.as_ref().unchecked_ref()).unwrap();
+  scroll_event.forget();
 }
